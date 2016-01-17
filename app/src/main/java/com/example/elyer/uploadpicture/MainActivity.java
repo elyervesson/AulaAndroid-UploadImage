68345a0e8315b7b21b95f30a822ee0347e26f1a3
Elyervesson de Souza <elyervesson@gmail.com>
2016-01-17 17:52:36 -0300

    Parte da aplicação que envia fotos para um servidor

---
+++ app/src/main/java/com/example/elyer/uploadpicture/MainActivity.java
@@ -0,0 +1,129 @@
+package com.example.elyer.uploadpicture;
+
+import org.apache.http;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private static final String SERVER_ADRESS = "http://elyervessontest.netne.net";
+
+    ImageView imageToUpload, imageToDownlad;
+    Button bUploadImage, bDonwloadImage;
+    EditText uploadImageName, downloadImageName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        imageToDownlad = (ImageView) findViewById(R.id.imageToDownload);
+
+        bUploadImage = (Button) findViewById(R.id.bUploadImage);
+        bDonwloadImage = (Button) findViewById(R.id.bDownloadImage);
+
+        uploadImageName = (EditText) findViewById(R.id.etUploadName);
+        downloadImageName = (EditText) findViewById(R.id.etDownloadName);
+
+        //Listeners Upload
+        imageToUpload.setOnClickListener(this);
+        bUploadImage.setOnClickListener(this);
+        uploadImageName.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageToUpload:
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);//Abrir a galeria
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                break;
+            case R.id.bUploadImage:
+                Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
+                new UploadImage(image, uploadImageName.getText().toString()).execute();
+                break;
+            case R.id.etUploadName:
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Verifica se o "onActivityResult" foi chamado pelo nosso listener E se a operação ocorreu
+        //corretamente E se a imagem não é nula
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();//Recupera a imagem
+            imageToUpload.setImageURI(selectedImage);//Exibe a imagem
+        }
+    }
+
+    private class UploadImage extends AsyncTask<Void, Void, Void> {
+
+        Bitmap image;
+        String name;
+        private static final int QUALIDADE_IMAGEM = 100;
+
+        public UploadImage(Bitmap image, String name) {
+            this.image = image;
+            this.name = name;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(); //Host da representação em bytes do bitmap
+            image.compress(Bitmap.CompressFormat.JPEG, QUALIDADE_IMAGEM, byteArrayOutputStream);
+
+            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);// Representação em string do ByteArrayOutputStream
+            ArrayList<NameValuePair> dataToSend = new ArrayList<>();
+            dataToSend.add(new BasicNameValuePair("image", encodedImage));
+            dataToSend.add(new BasicNameValuePair("name", name));
+
+            HttpParams httpRequestParams = getHttpRequestParams();
+
+            HttpClient client = new DefaultHttpClient(httpRequestParams);
+            HttpPost post = new HttpPost(SERVER_ADRESS + SavePicture.php);
+
+            try{
+                post.setEntity(new UrlEncodedFormEntity(dataToSend));
+                client.execute(post);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+    }
+
+    private HttpParams getHttpRequestParams() {
+        HttpParams httpRequestParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpRequestParams, 1000 * 30);
+        HttpConnectionParams.setSoTimeout(httpRequestParams, 1000 * 30);
+
+        return httpRequestParams;
+    }
+}
